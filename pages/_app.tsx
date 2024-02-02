2bac6f57a66e1b3397e4b02c5fbd9f447291a5d7
diddy032 <gwul3vu032@gmail.com>
2024-02-03 01:14:03 +0800

    app.tsx:設定meta、引入全域sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
-// import "@/styles/globals.css";
 import Head from "next/head";
 import LayoutIndex from "@/component/layout";
 import type { AppProps } from "next/app";
+import "normalize.css";
+import "@/styles/globals.sass";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>旅館網站</title>
-        <meta name="description" content="旅館,訂房" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="旅館,訂房" />
+        <meta name="key" content="旅館,訂房" />
+        <meta property="og:title" content="旅館網站" />
+        <meta property="og:description" content="旅館,訂房" />
+        <meta property="og:image" content="" />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <LayoutIndex>
