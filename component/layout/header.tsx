e85a4a2f452e7d1e1c8c775a6d66b7f5bb9bf3b0
diddy032 <gwul3vu032@gmail.com>
2024-02-03 23:30:55 +0800

    header: 切版&RWD

--- component/layout/header.tsx
+++ component/layout/header.tsx
@@ -1,5 +1,99 @@
-import React from "react";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import style from "./styles/header.module.sass";
 
-export default function Header() {
-  return <div>Header</div>;
+interface HeaderProps {
+  isTransparentBg?: boolean;
+  showNav?: boolean;
+}
+
+export default function Header({
+  isTransparentBg,
+  showNav = true,
+}: HeaderProps) {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  return (
+    <>
+      <div
+        className={`${style.container} ${
+          isTransparentBg ? style["ts-bg"] : ""
+        }`}
+      >
+        <div>
+          <Link href="/">
+            <Image
+              priority
+              width={110}
+              height={40}
+              alt="網站LOGO"
+              src="/images/logo.png"
+              layout="responsive"
+              sizes="(min-width: 992px) 196px, 72px"
+            />
+          </Link>
+        </div>
+        <div
+          className={`${style["nav-list-web"]} ${showNav ? "" : style.hidden}`}
+        >
+          <Link href="/" className={style["nav-item"]}>
+            客房旅館
+          </Link>
+          <Link href="/member/login" className={style["nav-item"]}>
+            會員登入
+          </Link>
+          <Link
+            href="/"
+            className={`${style["nav-item"]} ${style["btn-style"]}`}
+          >
+            立即訂房
+          </Link>
+        </div>
+        <button
+          type="button"
+          className={style["nav-btn-mobile"]}
+          onClick={() => setIsOpenMenu(!isOpenMenu)}
+        >
+          <Image
+            src="/images/icons/icon_menu.svg"
+            width={24}
+            height={24}
+            alt="開啟手機選單"
+          />
+        </button>
+      </div>
+      <div
+        className={`${style["nav-mobile-frame"]} ${
+          isOpenMenu ? style.active : ""
+        }`}
+      >
+        <button
+          type="button"
+          onClick={() => setIsOpenMenu(false)}
+          className={style["nav-btn-mobile-close"]}
+        >
+          <Image
+            src="/images/icons/icon_close.svg"
+            width={48}
+            height={48}
+            alt="關閉手機選單"
+          />
+        </button>
+        <div className={style["mobile-nav-list"]}>
+          <Link href="/" className={style["nav-item"]}>
+            客房旅館
+          </Link>
+          <Link href="/member/login" className={style["nav-item"]}>
+            會員登入
+          </Link>
+          <Link
+            href="/"
+            className={`${style["nav-item"]} ${style["btn-style"]}`}
+          >
+            立即訂房
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
