839aa9fc8ac8af86709169ae8c9ada6ffa4c88b6
diddy032 <gwul3vu032@gmail.com>
2024-02-04 22:36:13 +0800

    app.tsx：偵測是否登入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,29 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import Context from "@/context";
+import { useEffect, useState } from "react";
+import { getCookie, deleteCookie } from "cookies-next";
+import { fetchDataForToken } from "@/api/helper";
+
 import "normalize.css";
 import "@/styles/globals.sass";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [authToken, setAuthToken] = useState<string>("");
+  const [account, setAccount] = useState<{}>({});
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  async function checkLogin() {
+    const haveToken = getCookie("hotel");
+    if (haveToken) {
+      let obj = await fetchDataForToken("/api/v1/user/check", haveToken);
+      if (!obj?.status) !obj?.status && deleteCookie("hotel");
+      if (obj?.status) setAuthToken(haveToken);
+    }
+  }
+
   return (
     <>
       <Head>
@@ -22,7 +41,7 @@ export default function App({ Component, pageProps }: AppProps) {
           url('https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@200;300;400;500;600;700;900&display=swap');
         </style>
       </Head>
-      <Context>
+      <Context authTokenProps={authToken}>
         <Component {...pageProps} />
       </Context>
     </>
